1521962e34c56923212a938bc3d02d96dd929bd7
Yohanan-Negash <yoniget066@gmail.com>
2024-11-21 22:31:41 -0600

    Implement sidebar + move sign out logic to it

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,3 +1,18 @@
+import { SidebarProvider, SidebarTrigger } from '../../components/ui/sidebar';
+import { AppSidebar } from '../../components/app-sidebar';
+import { Button } from '../../components/ui/button';
+
 export default function Layout({ children }) {
-  return <>{children}</>;
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <main className='flex flex-col flex-1'>
+        <div className='flex justify-between items-center p-2 border-b'>
+          <SidebarTrigger />
+          <Button variant='default'>Upgrade</Button>
+        </div>
+        {children}
+      </main>
+    </SidebarProvider>
+  );
 }
---
+++ components/app-sidebar.jsx
@@ -0,0 +1,175 @@
+import {
+  Calendar,
+  Home,
+  Inbox,
+  ChevronDown,
+  User2,
+  LogOut,
+  Settings,
+  Send,
+  Mail,
+  DollarSign,
+} from 'lucide-react';
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarFooter,
+} from '../components/ui/sidebar';
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+} from './ui/dropdown-menu';
+import { Separator } from '../components/ui/separator';
+import { redirect } from 'next/navigation';
+import { logout } from '../lib/actions';
+import { Button } from '../components/ui/button';
+import { createClient } from '../lib/supabase/server';
+
+//Application section items.
+const applicationItems = [
+  {
+    title: 'Home',
+    url: '#',
+    icon: Home,
+  },
+  {
+    title: 'Inbox',
+    url: '#',
+    icon: Inbox,
+  },
+  {
+    title: 'Calendar',
+    url: '#',
+    icon: Calendar,
+  },
+];
+
+//Help section items.
+const helpItems = [
+  {
+    title: 'Support',
+    url: '#',
+    icon: Mail,
+  },
+  {
+    title: 'Feedback',
+    url: '#',
+    icon: Send,
+  },
+];
+
+//Earn Section Item
+const earnItem = {
+  title: 'Become an Affiliate',
+  url: '/affiliate',
+  icon: DollarSign,
+};
+
+export async function AppSidebar() {
+  const supabase = createClient();
+
+  const { data: userData, error: userError } = await supabase.auth.getUser();
+
+  if (userError || !userData?.user) {
+    console.error('Error getting user:', userError.message);
+    redirect('/login');
+  }
+
+  //Extract the user's email & id from the userData
+  const userEmail = userData.user.email;
+
+  return (
+    <Sidebar collapsible='icon'>
+      <Separator className='mb-1' />
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {applicationItems.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+        <SidebarGroup>
+          <SidebarGroupLabel>Help</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {helpItems.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+        <SidebarGroup>
+          <SidebarGroupLabel>Earn</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                  <a href={earnItem.url}>
+                    <earnItem.icon />
+                    <span>{earnItem.title}</span>
+                  </a>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <SidebarMenuButton>
+                  <User2 /> {userEmail}
+                  <ChevronDown className='ml-auto' />
+                </SidebarMenuButton>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent
+                side='top'
+                className='w-[--radix-popper-anchor-width]'
+              >
+                <DropdownMenuItem>
+                  <Settings />
+                  <span>Settings</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <LogOut />
+                  <form action={logout}>
+                    <Button type='submit'>Sign out</Button>
+                  </form>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
