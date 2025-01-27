e20440dc4ab25e0c55101e5d231427a862061aa8
Yohanan-Negash <yoniget066@gmail.com>
2025-01-27 12:09:32 -0600

    display user full name

--- components/nav-user.jsx
+++ components/nav-user.jsx
@@ -39,13 +39,18 @@ export function NavUser({ user }) {
               className='data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground'
             >
               <Avatar className='h-8 w-8 rounded-lg'>
-                <AvatarImage src={user.avatar} alt={user.name} />
+                <AvatarImage
+                  src={user.avatar}
+                  alt={user.user_metadata.full_name}
+                />
                 <AvatarFallback className='rounded-lg'>
                   <User2 />
                 </AvatarFallback>
               </Avatar>
               <div className='grid flex-1 text-left text-sm leading-tight'>
-                <span className='truncate font-semibold'>{user.name}</span>
+                <span className='truncate font-semibold'>
+                  {user.user_metadata.full_name}
+                </span>
                 <span className='truncate text-xs'>{user.email}</span>
               </div>
               <ChevronsUpDown className='ml-auto size-4' />
@@ -60,13 +65,18 @@ export function NavUser({ user }) {
             <DropdownMenuLabel className='p-0 font-normal'>
               <div className='flex items-center gap-2 px-1 py-1.5 text-left text-sm'>
                 <Avatar className='h-8 w-8 rounded-lg'>
-                  <AvatarImage src={user.avatar} alt={user.name} />
+                  <AvatarImage
+                    src={user.avatar}
+                    alt={user.user_metadata.full_name}
+                  />
                   <AvatarFallback className='rounded-lg'>
                     <User2 />
                   </AvatarFallback>
                 </Avatar>
                 <div className='grid flex-1 text-left text-sm leading-tight'>
-                  <span className='truncate font-semibold'>{user.name}</span>
+                  <span className='truncate font-semibold'>
+                    {user.user_metadata.full_name}
+                  </span>
                   <span className='truncate text-xs'>{user.email}</span>
                 </div>
               </div>
