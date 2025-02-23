ea40a676e65e796cc3bb98d9720e0cf5480bbe3f
Yohanan-Negash <yoniget066@gmail.com>
2025-02-23 17:36:25 -0600

    add shiny border over the internships section

--- components/landingpage/heroscroll.jsx
+++ components/landingpage/heroscroll.jsx
@@ -2,26 +2,35 @@
 import React from 'react';
 import CompanyCarousel from '../../components/landingpage/CompanyCarousel';
 import Image from 'next/image';
+import { ShineBorder } from '../../components/ui/shineborder';
 
 const HeroScroll = () => {
   return (
     <div className='flex flex-col overflow-hidden'>
-      <div className='flex flex-col items-center justify-center'>
-        <h1 className='font-ubuntu text-2xl text-center text-cyan-300'>
-          Never miss an Opportunity <br />
-          <span className='font-ubuntu text-4xl md:text-6xl font-bold mt-4 block leading-none bg-gradient-to-r from-purple-400 to-pink-400 bg-clip-text text-transparent mb-4'>
-            150+ Internships
-          </span>
-        </h1>
-        <Image
-          src='https://kkpqypbvgqiygycagfey.supabase.co/storage/v1/object/public/video-image-storage/comparison-images-videos/internship_screenshot.png'
-          alt='Hero'
-          width={1400}
-          height={720}
-          className='mx-auto rounded-2xl object-cover h-full object-left-top'
-          draggable={false}
-        />
-      </div>
+      <h1 className='font-ubuntu text-2xl text-center text-cyan-300'>
+        Never miss an Opportunity <br />
+        <span className='font-ubuntu text-4xl md:text-6xl font-bold mt-4 block leading-none bg-gradient-to-r from-purple-400 to-pink-400 bg-clip-text text-transparent mb-4'>
+          150+ Internships
+        </span>
+      </h1>
+      <ShineBorder
+        borderRadius={20}
+        borderWidth={2}
+        duration={10}
+        color={['#A07CFE', '#FE8FB5', '#FFBE7B']}
+        className='w-full h-full p-1 bg-transparent'
+      >
+        <div className='flex flex-col items-center justify-center'>
+          <Image
+            src='https://kkpqypbvgqiygycagfey.supabase.co/storage/v1/object/public/video-image-storage/comparison-images-videos/internship_screenshot.png'
+            alt='Hero'
+            width={1400}
+            height={720}
+            className='mx-auto rounded-2xl object-cover h-full object-left-top'
+            draggable={false}
+          />
+        </div>
+      </ShineBorder>
       <CompanyCarousel />
     </div>
   );
